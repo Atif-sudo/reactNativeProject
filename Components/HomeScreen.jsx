5312da6764a60afa1daf15a0f00f0403417f3436
Mohammed Atif Ansari <atifa7747@gmail.com>
2023-07-24 18:29:06 +0530

    Change in product

--- Components/HomeScreen.jsx
+++ Components/HomeScreen.jsx
@@ -1,97 +1,223 @@
+import React from "react";
 
-import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  ToastAndroid,
+} from "react-native";
+class HomeScreen extends React.Component {
+  state = {
+    q1: "",
+    a1: "",
+    q2: "",
+    a2: "",
+    q3: "",
+    a3: "",
+    disp1: false,
+    disp2: false,
+    disp3: false,
+    tot: 0,
+  };
 
-const HomeScreen = () => {
-return (
-    <View style={styles.container}>
-      {/* First box */}
-    <View style={[styles.box, styles.box1]}>
-        {/* Internal container 1 */}
-        <View style={styles.internalContainer1}>
-          {/* Box 3 */}
-        <View style={[styles.internalBox, styles.box3, ]}>
-            <Text style={[styles.setColorWhite, styles.textFirst]}>Product 1</Text>
-            <Text style={[styles.setColorWhite , styles.textSecond]}>₹ 199.99</Text>
-        </View>
-        
-          {/* Box 4 */}
-        <View style={[styles.internalBox, styles.box4, styles.inputtext]}>
-           
-            <TextInput placeholder='Qty' style={styles.input} />
-        </View>
-        </View>
-        
-        {/* Internal container 2 */}
-        <View style={styles.internalContainer2}>
-          {/* Box 5 */}
-        <View style={[styles.internalBox, styles.box3]}>
-            <Text style={[styles.setColorWhite, styles.textFirst]}>Product 2</Text>
-            <Text style={[styles.setColorWhite, styles.textSecond]}>₹ 199.99</Text>
-          </View>
-            
-          {/* Box 6 */}
-          <View style={[styles.internalBox, styles.box4,  styles.inputtext]}>
-          <TextInput placeholder='Qty' style={styles.input} />
-          </View>
-        </View>
+  render() {
+    return (
+      <View>
+        <View style={styles.container}>
+          {/* First box */}
+          <View style={[styles.box, styles.box1]}>
+            {/* Internal container 1 */}
+            <View style={styles.internalContainer1}>
+              {/* Box 3 */}
+              <View style={[styles.internalBox, styles.box3]}>
+                <Text style={[styles.setColorWhite, styles.textFirst]}>
+                  Product 1
+                </Text>
+                <Text style={[styles.setColorWhite, styles.textSecond]}>
+                  ₹ 199.99
+                </Text>
+              </View>
 
-        {/* Internal container 2 */}
-        <View style={styles.internalContainer2}>
-          {/* Box 5 */}
-        <View style={[styles.internalBox, styles.box3]}>
-            <Text style={[styles.setColorWhite, styles.textFirst]}>Product 3</Text>
-            <Text style={[styles.setColorWhite, styles.textSecond]}>₹ 199.99</Text>
+              {/* Box 4 */}
+              <View style={[styles.internalBox, styles.box4, styles.inputtext]}>
+                <TextInput
+                  placeholder="Qty"
+                  style={styles.input}
+                  onChangeText={(value) =>
+                    this.setState({
+                      q1: value,
+                      a1: parseFloat(value) * 199.99,
+                      disp1: value.length > 0 ? true : false,
+                      tot:
+                        parseFloat(value) * 199.99 +
+                        this.state.a2 +
+                        this.state.a3,
+                    })
+                  }
+                  value={this.state.q1}
+                />
+              </View>
+            </View>
+
+            {/* Internal container 2 */}
+            <View style={styles.internalContainer2}>
+              {/* Box 5 */}
+              <View style={[styles.internalBox, styles.box3]}>
+                <Text style={[styles.setColorWhite, styles.textFirst]}>
+                  Product 2
+                </Text>
+                <Text style={[styles.setColorWhite, styles.textSecond]}>
+                  ₹ 199.99
+                </Text>
+              </View>
+
+              {/* Box 6 */}
+              <View style={[styles.internalBox, styles.box4, styles.inputtext]}>
+                <TextInput
+                  placeholder="Qty"
+                  style={styles.input}
+                  onChangeText={(value) =>
+                    this.setState({
+                      q2: value,
+                      a2: parseFloat(value) * 199.99,
+                      disp2: value.length > 0 ? true : false,
+                      tot:
+                        parseFloat(value) * 199.99 +
+                        this.state.a1 +
+                        this.state.a3,
+                    })
+                  }
+                  value={this.state.q2}
+                />
+              </View>
+            </View>
+
+            {/* Internal container 2 */}
+            <View style={styles.internalContainer2}>
+              {/* Box 5 */}
+              <View style={[styles.internalBox, styles.box3]}>
+                <Text style={[styles.setColorWhite, styles.textFirst]}>
+                  Product 3
+                </Text>
+                <Text style={[styles.setColorWhite, styles.textSecond]}>
+                  ₹ 199.99
+                </Text>
+              </View>
+
+              {/* Box 6 */}
+              <View style={[styles.internalBox, styles.box4, styles.inputtext]}>
+                <TextInput
+                  placeholder="Qty"
+                  style={styles.input}
+                  onChangeText={(value) =>
+                    this.setState({
+                      q3: value,
+                      a3: parseFloat(value) * 199.99,
+                      disp3: value.length > 0 ? true : false,
+                      tot:
+                        parseFloat(value) * 199.99 +
+                        this.state.a1 +
+                        this.state.a2,
+                    })
+                  }
+                  value={this.state.q3}
+                />
+              </View>
+            </View>
           </View>
-            
-          {/* Box 6 */}
-          <View style={[styles.internalBox, styles.box4,  styles.inputtext]}>
-          <TextInput placeholder='Qty' style={styles.input} />
+
+          {/* Second box */}
+          <View style={[styles.box, styles.box2]}>
+            {/* Table header */}
+            <View style={styles.tableRow}>
+              <Text style={[styles.headerCell, styles.tableStyle]}>
+                Product
+              </Text>
+              <Text style={[styles.headerCell, styles.tableStyle]}>Qty</Text>
+              <Text style={[styles.headerCell, styles.tableStyle]}>Rate</Text>
+              <Text style={[styles.headerCell, styles.tableStyle]}>Amt</Text>
+            </View>
+
+            {/* Table Rows */}
+            <View style={[this.state.disp1 ? styles.tableRow : styles.hidden]}>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                Product 1
+              </Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                {this.state.q1 + ""}
+              </Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>199.99</Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                {this.state.a1 + ""}
+              </Text>
+            </View>
+            <View style={[this.state.disp2 ? styles.tableRow : styles.hidden]}>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                Product 2
+              </Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                {this.state.q2 + ""}
+              </Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>199.99</Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                {this.state.a2 + ""}
+              </Text>
+            </View>
+            <View style={[this.state.disp3 ? styles.tableRow : styles.hidden]}>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                Product 3
+              </Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                {this.state.q3 + ""}
+              </Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>199.99</Text>
+              <Text style={[styles.dataCell, styles.tableStyle]}>
+                {this.state.a3 + ""}
+              </Text>
+            </View>
+            <View>
+              <Text style={[styles.totalBox]}>
+                Total: {this.state.tot + ""} INR
+              </Text>
+            </View>
+          </View>
+          <View>
+            <Button
+              style={styles.submitbutton}
+              title="Submit"
+              onPress={() => {
+                return;
+
+                //Handle LOGIN
+              }}
+            />
           </View>
         </View>
       </View>
-      
-      {/* Second box */}
-      <View style={[styles.box, styles.box2]}>
-        {/* Table header */}
-        <View style={styles.tableRow}>
-          <Text style={[styles.headerCell, styles.tableStyle]}>Product</Text>
-          <Text style={[styles.headerCell, styles.tableStyle]}>Qty</Text>
-          <Text style={[styles.headerCell, styles.tableStyle]}>Rate</Text>
-          <Text style={[styles.headerCell, styles.tableStyle]}>Amt</Text>
-        </View>
-
-        {/* Table Rows */}
-        <View style={styles.tableRow}>
-          <Text style={[styles.dataCell ,styles.tableStyle]}>Product1</Text>
-          <Text style={[styles.dataCell ,styles.tableStyle]}>2</Text>
-          <Text style={[styles.dataCell ,styles.tableStyle]}>199.99</Text>
-          <Text style={[styles.dataCell ,styles.tableStyle]}>399.9800</Text>
-        </View>
-      </View>
-    </View>
-  );
-};
-
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: 'aliceblue',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    backgroundColor: "aliceblue",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
     borderWidth: 1,
     borderRadius: 2,
   },
   internalContainer1: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 8,
     marginTop: 10,
   },
   internalContainer2: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 8,
     marginTop: -395,
   },
@@ -100,101 +226,108 @@ const styles = StyleSheet.create({
     height: 590,
   },
   box1: {
-    
-    flexDirection: 'column',
+    flexDirection: "column",
     borderWidth: 1,
     borderRadius: 4,
   },
   box2: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 1,
     borderRadius: 4,
+    flexWrap: "wrap",
   },
   internalBox: {
     width: 64,
     height: 56,
     borderRadius: 4,
-    
-    
   },
   box3: {
-    backgroundColor: 'dodgerblue',
-    color: 'white',
+    backgroundColor: "dodgerblue",
+    color: "white",
     padding: 2,
     width: 78,
     zIndex: 1,
-    
   },
-  inputtext:{
-    
+  inputtext: {
     padding: 12,
     width: 75,
     left: -10,
-    
-    
   },
-  textFirst:{
-    textAlign: 'center',
+  textFirst: {
+    textAlign: "center",
     top: 3,
   },
-  textSecond:{
-    textAlign: 'right',
+  textSecond: {
+    textAlign: "right",
     top: 10,
     fontSize: 10,
   },
 
   setColorWhite: {
-    color: 'white',
+    color: "white",
   },
   box4: {
-    
-    backgroundColor: 'gainsboro',
-    
+    backgroundColor: "gainsboro",
   },
-  
+
   input: {
     height: 30,
     marginLeft: 14,
     borderWidth: 2,
     padding: 3,
     borderRadius: 4,
-    borderColor: 'darkgrey',
+    borderColor: "darkgrey",
   },
   button: {
     height: 50,
-    backgroundColor: 'lightcoral',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "lightcoral",
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 14,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   tableRow: {
     paddingTop: 9,
     paddingLeft: 3,
-    paddingBottom: 0,
-    
-    flexDirection: 'row',
-  },
-  tableStyle:{
-    fontSize: 11,
-    paddingTop: 10,
-    borderColor: 'darkgrey',
+    flexDirection: "row",
+    flex: 0,
 
-    textAlign: 'center',
+    textAlign: "center",
   },
   headerCell: {
     flex: 1,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     borderWidth: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   dataCell: {
     flex: 1,
     borderWidth: 1,
   },
-});
+  tableStyle: {
+    fontSize: 11,
+    paddingTop: 10,
+    borderColor: "darkgrey",
 
+    textAlign: "center",
+  },
+  totalBox: {
+    padding: 10,
+    borderWidth: 2,
+    top: 120,
+    position: "relative",
+  },
+
+  hidden: {
+    width: 0,
+    height: 0,
+  },
+  submitbutton: {
+    left: -20,
+    top: 200,
+  },
+});
 export default HomeScreen;
--- Components/Navigation.jsx
+++ Components/Navigation.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './HomeScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LoginPage from "./HomeScreen";
+import HomeScreen from "./HomeScreen";
 
 const Stack = createStackNavigator();
 
 const Navigation = () => {
-return (
+  return (
     <NavigationContainer>
-    <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        
-    </Stack.Navigator>
+      <Stack.Navigator>
+        {
+          <Stack.Screen name="Home" component={HomeScreen} />
+          /* <Stack.Screen name="Home" component={HomeScreen} /> */
+        }
+      </Stack.Navigator>
     </NavigationContainer>
-);
+  );
 };
 
 export default Navigation;
